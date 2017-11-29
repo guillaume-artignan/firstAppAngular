0b6c92f2870fe191e47406b3a491be5521d56ec5
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2017-11-29 12:46:10 +0100

    Exemple en Objet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
 
+class Personne{
+	nom : String;
+	prenom : String;
+	description : String;
+	travail : String;
+	
+	constructor(nom : String, prenom : String, description : String, travail : String){
+		this.nom = nom;
+		this.prenom = prenom;
+		this.description = description;
+		this.travail = travail;
+	}
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,9 +23,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Liste des Scientifiques';
   
-  
-  nom = "Einstein";
-  prenom = "Albert";
-  description ="Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.";
-  travail = "Relativité Générale";
+  personne = new Personne("Einstein",
+				"Albert",
+				"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
+				"Relativité Générale");
+
+	/*personne = {
+				nom :"Einstein",
+				prenom : "Albert",
+				description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
+				travail : "Relativité Générale"
+			};	*/	
 }
