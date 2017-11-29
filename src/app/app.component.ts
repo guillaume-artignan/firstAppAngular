05a7471dd54044a94478af7d4ccd71c75574ba45
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2017-11-29 12:48:14 +0100

    Developpement des Objets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,10 @@ export class AppComponent {
   title = 'Liste des Scientifiques';
   
   
-  nom = "Einstein";
-  prenom = "Albert";
-  description ="Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.";
-  travail = "Relativité Générale";
+  personne = {
+				nom :"Einstein",
+				prenom : "Albert",
+				description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
+				travail : "Relativité Générale"
+			};	
 }
