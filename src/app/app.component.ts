dec9db4c033792c5c9e1f224a414cb25421e876a
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2017-11-30 10:04:30 +0100

    Evenement Master/Detail

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,17 +14,9 @@ export class AppComponent {
 		  {nom :"Curie", prenom : "Marie", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"},
           {nom :"Curie", prenom : "Pierre", description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.", travail : "Relativité Générale"}]
   
-  personne = {
-				nom :"Einstein",
-				prenom : "Albert",
-				description :"Il publie sa théorie de la relativité restreinte en 1905 et sa théorie de la gravitation dite relativité générale en 1916. Il contribue largement au développement de la mécanique quantique et de la cosmologie, et reçoit le prix Nobel de physique de 1921 pour son explication de l’effet photoélectrique2. Son travail est notamment connu du grand public pour l’équation E=mc2, qui établit une équivalence entre la matière et l’énergie d’un système.",
-				travail : "Relativité Générale"
-			};	
+  personne = this.liste[0];	
 			
-  count = 0;
-  
-  clickEffectue = function(){
-		this.count++;
-		this.personne.nom = this.personne.nom +" "+ this.count;
+  clickPersonne = function(a){
+		this.personne = a;
   }
 }
