52646aea3c1225f5f5e4188c8fe722830a1d5a69
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2017-11-30 12:26:42 +0100

    Premier pas avec les composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { DetailComponent } from './detail/detail.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule, FormsModule
---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'detail',
+  templateUrl: './detail.component.html',
+  styleUrls: ['./detail.component.css']
+})
+export class DetailComponent implements OnInit {
+
+  alea = Math.random();
+  
+  constructor() { 
+  
+  
+  }
+
+  ngOnInit() {
+	  
+	  
+  }
+
+}
