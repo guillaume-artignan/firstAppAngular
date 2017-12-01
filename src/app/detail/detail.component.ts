b4fdadf8c464dd07836f7bcb0f002d3d8b72d8de
DESKTOP-GKRDK5D\Artignan <guillaume.artignan@gmail.com>
2017-12-01 09:47:41 +0100

    Injecter un service

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { CompteurService} from '../compteur.service';
 
 @Component({
   selector: 'detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers: [CompteurService]
 })
 export class DetailComponent implements OnInit {
-
-  
+ 
   @Input() maPersonne;
+   
+  constructor(private compteurService:CompteurService)  { }
   
-  
-  
-  constructor() {}
   ngOnInit() {}
   
-
+  clickSurTitre = function(){
+	  this.compteurService.ajoute();
+	  var v = this.compteurService.donne();
+	  console.log(v);
+  }
 }
